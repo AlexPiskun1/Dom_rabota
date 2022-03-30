6f3c1cc4e9eabe19c4b27c0a31e3480f70aecc52
Piskun_Alex <piskun.a@mail.ru>
2022-03-30 23:25:29 +0300

    V 5.0 near

--- Lesson_5/Calculator_l5.py
+++ Lesson_5/Calculator_l5.py
@@ -2,29 +2,34 @@
 
 n= 0
 
+
 while True:
-    a = input("Введите действие через пробел").split()
-    n +=1
-    if a[0] == "стоп":
-        break
-    elif a[1] == "+":
-        print("Ответ", float(a[0])+float(a[2]))
-        print("Номера вашей операции", n)
-    elif a[1] == "-":
-        print("Ответ", float(a[0])-float(a[2]))
-        print("Номера вашей операции", n)
-    elif a[1] == "*":
-        print("Ответ", float(a[0])*float(a[2]))
-        print("Номера вашей операции", n)
-    elif a[1] == "/":
-        if a[2] == "0":
-            print("На ноль не делится")
-        else:
-            print("Ответ", float(a[0]) / float(a[2]))
-            print("Номера вашей операции", n)
-    elif a[1] == "**":
-        print("Ответ", float(a[0])**float(a[2]))
-        print("Номера вашей операции", n)
-    else:
-        print("Некоректный ввод данных")
-        print("Номера вашей операции",n)
+
+        a = input("Введите действие через пробел").split()
+        n +=1
+        try:
+            if a[0] == "стоп":
+                break
+            elif a[1] == "+":
+                print("Ответ", float(a[0])+float(a[2]))
+                print("Номера вашей операции", n)
+            elif a[1] == "-":
+                print("Ответ", float(a[0])-float(a[2]))
+                print("Номера вашей операции", n)
+            elif a[1] == "*":
+                print("Ответ", float(a[0])*float(a[2]))
+                print("Номера вашей операции", n)
+            elif a[1] == "/":
+                if a[2] == "0":
+                    print("На ноль не делится")
+                else:
+                    print("Ответ", float(a[0]) / float(a[2]))
+                    print("Номера вашей операции", n)
+            elif a[1] == "**":
+                print("Ответ", float(a[0])**float(a[2]))
+                print("Номера вашей операции", n)
+            else:
+                print("Некоректный ввод данных")
+                print("Номера вашей операции",n)
+        except:
+            print("Неверный ввод данных")

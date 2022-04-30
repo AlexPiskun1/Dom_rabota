a9f942a62455f3d807fba4872732d506ea9493dc
Alex_Piskun <piskun.a@mail.ru>
2022-04-30 11:00:39 +0300

    V1.3

--- Lesson_13/Bot_Weather.py
+++ Lesson_13/Bot_Weather.py
@@ -8,18 +8,27 @@ bot = telebot.TeleBot(key_weather.key_bot())
 def start_message(message):
     bot.send_message(message.chat.id, "Добро пожаловать, я поГОДНЫЙ бот")
 
+
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     text = message.text.lower()
-    if text in ["привет", "добрый день", "здоров", "hello", "hi"]:
-        bot.send_message(message.chat.id, "Привет прямоходящий, ВВЕДИ название города")
-    else:
-        sity = str(text)
-        owm = OWM(key_weather.key_w())
-        mgr = owm.weather_manager()
-        observation = mgr.weather_at_place(sity)
-        w = observation.weather
-        bot.send_message(message.chat.id, f"Температура: {w.temperature('celsius')['temp']} градусов \n Ветер: {w.wind()['speed']} м/с \n Облачность: {w.clouds} %")
+    try:
+        if text in ["привет", "добрый день", "здоров", "hello", "hi"]:
+            bot.send_message(message.chat.id, "Привет прямоходящий, ВВЕДИ название города")
+
+        else:
+            sity = str(text)
+            owm = OWM(key_weather.key_w())
+            mgr = owm.weather_manager()
+            observation = mgr.weather_at_place(sity)
+            w = observation.weather
+            bot.send_message(message.chat.id, f"Температура: {w.temperature('celsius')['temp']} градусов \n Ветер: {w.wind()['speed']} м/с \n Облачность: {w.clouds} %")
+    except Exception:
+        bot.send_message(message.chat.id, "Вы неправильно ввели название города, введите еще раз")
+
+
+
 
 
 
